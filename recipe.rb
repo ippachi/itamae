6d4fe2319875289e120fc4e276d5f6a38975c16b
ippachi <ivalice1218@gmail.com>
2019-12-04 08:58:01 +0000

    Itamae

---
+++ recipe.rb
@@ -0,0 +1,14 @@
+aur_package 'base-devel'
+aur_package 'exa'
+aur_package 'vim'
+aur_package 'lazygit'
+aur_package 'git'
+aur_package 'rbenv'
+aur_package 'ruby-build'
+aur_package 'nvm'
+
+aur_package 'docker'
+aur_package 'docker-compose'
+aur_package 'lazydocker'
+
+aur_package 'tig'
